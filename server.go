0f2391c47a5c63cefed3e2e944a9f711809db6f4
August <augustt198@gmail.com>
2015-06-13 17:17:25 -0400

    Initial commit

---
+++ server.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+  "github.com/go-martini/martini"
+  "github.com/martini-contrib/render"
+)
+
+func main() {
+  m := martini.Classic()
+
+  m.Use(render.Renderer(render.Options{
+    Layout: "layout",
+  }))
+
+  m.Get("/", func(r render.Render) {
+    r.HTML(200, "layout", nil)
+  })
+
+  m.Run()
+}
