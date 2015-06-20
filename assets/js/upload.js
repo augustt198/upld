a4c772cc69db8d18d4f246f6e617e79b94c8936f
August <augustt198@gmail.com>
2015-06-20 15:42:23 -0400

    Drag files

--- assets/js/upload.js
+++ assets/js/upload.js
@@ -1,64 +1,7 @@
 // http://blog.teamtreehouse.com/uploading-files-ajax
-var form = document.getElementById("upload-form");
-var fileSelector = document.getElementById("file-selector");
-var uploadButton = document.getElementById("upload-button");
 
 // 20MB
 var MAX_SIZE = 20 * 1000000;
-
-form.onsubmit = function(e) {
-    e.preventDefault();
-
-    var file = fileSelector.files[0];
-    if (file.size > MAX_SIZE) {
-        alert("File above maximum size: 20MB");
-        return;
-    }
-    
-    var formData = new FormData();
-    formData.append("upload", file, file.name);
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", "/upload", true);
-    xhr.upload.addEventListener("progress", updateProgress, false);
-
-    uploadButton.setAttribute("value", "Uploading 0%");
-    var container = document.getElementById("progress-bar-container");
-    clearChildren(container);
-    var bar = document.createElement("div");
-    bar.setAttribute("class", "progress-bar");
-    var fill = document.createElement("div");
-    fill.setAttribute("class", "progress-bar-fill");
-    fill.setAttribute("style", "width: 0")
-    bar.appendChild(fill);
-    container.appendChild(bar);
-
-    function updateProgress(event) {
-        if (event.lengthComputable) {
-            var raw = event.loaded / event.total
-            var width = Math.round(raw * 298);
-            var percent = Math.round(raw * 100);
-            
-            fill.setAttribute("style", "width: " + width + "px");
-            if (percent == 100) {
-                uploadButton.setAttribute("value", "Processing...");
-            } else {
-                uploadButton.setAttribute("value", "Uploading " + percent + "%");
-            }
-        }
-    }
-
-    xhr.onload = function() {
-        if (xhr.status == 200) {
-            window.location = "/view/" + xhr.responseText;
-        } else {
-            uploadButton.setAttribute("class", "btn btn-danger");
-            uploadButton.setAttribute("value", "Error")
-            console.log("Error: " + xhr.status);
-        }
-    }
-
-    xhr.send(formData);
-}
 
 function addFiles() {
     var children = document.getElementById("hidden-inputs-wrapper").children;
@@ -118,7 +61,6 @@ function uploadFile(file) {
                 progressSpan.innerHTML = percent + "%"
                 progressBar.setAttribute("style", "width: " + (raw * 100) + "%");    
             }
-            
         }
     }
 
@@ -141,4 +83,41 @@ function uploadFile(file) {
     }
 
     xhr.send(data);
+}
+
+var htmlNode = document.body.parentNode;
+htmlNode.ondragover = dragEnter;
+htmlNode.ondragleave = dragLeave;
+htmlNode.ondrop = onDrop;
+
+var isDragging = false;
+var titleElem = document.getElementById("upload-list-title");
+
+function dragEnter(e) {
+    e.stopPropagation();
+    e.preventDefault();
+
+    if (!isDragging) {
+        titleElem.innerHTML = "Release to upload";
+        isDragging = true;
+    }
+
+}
+
+function dragLeave(e) {
+    e.stopPropagation();
+    e.preventDefault();
+    
+    isDragging = false;
+    titleElem.innerHTML = "Upload files";
+}
+
+function onDrop(e) {
+    e.stopPropagation();
+    e.preventDefault();
+
+    files = e.dataTransfer.files;
+    for (var i = 0; i < files.length; i++) {
+        uploadFile(files[i]);
+    }
 }
